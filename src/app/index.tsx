9b98804d6575a0f8fa8666eb98a9d76b16a286d3
Aymenbachiri <aymen.bachiri99@gmail.com>
2024-06-22 20:05:30 +0200

    add responsive

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,15 @@
 import { View, Text, TextInput } from "react-native";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
 
 export default function Home() {
   return (
-    <View className="flex-1 justify-center items-center">
+    <View
+      style={{ height: hp(100), width: wp(100) }}
+      className="flex-1 justify-center items-center"
+    >
       <Text className="text-3xl mb-6">Home</Text>
       <TextInput
         className="border w-1/2 rounded-md bg-slate-400 p-4"
